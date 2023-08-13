c281c0aef0dbceeb81711cdf548bceb75aecf445
Scott Vincent <scottvincent@yahoo.com>
2023-08-13 12:30:05 +0100

    New version

--- flightsim-charts/src/flightsim-charts.cpp
+++ flightsim-charts/src/flightsim-charts.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <thread>
 
-const char* versionString = "v2.0.1";
+const char* versionString = "v2.0.2";
 bool _quit = false;
 bool _showAi = false;
 bool _noConnect = false;
